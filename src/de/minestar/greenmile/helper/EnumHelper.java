7fc2e15303b8600716a19dffb74e1da16a5312c0
GeMoschen <x-chris-x@arcor.de>
2012-01-19 13:57:07 +0100

    - readded comments to EnumHelper.java

--- src/de/minestar/greenmile/helper/EnumHelper.java
+++ src/de/minestar/greenmile/helper/EnumHelper.java
@@ -4,6 +4,12 @@ import org.bukkit.Difficulty;
 import org.bukkit.World.Environment;
 
 public class EnumHelper {
+    /**
+     * Get the World.Environment by the name
+     * 
+     * @param text
+     * @return the environment, or null if not found
+     */
     public static Environment getEnvironment(String text) {
         for (Environment environment : Environment.values()) {
             if (text.equalsIgnoreCase(environment.toString()))
@@ -12,6 +18,12 @@ public class EnumHelper {
         return null;
     }
 
+    /**
+     * Get the World.Difficulty by the name
+     * 
+     * @param text
+     * @return the difficulty, or null if not found
+     */
     public static Difficulty getDifficulty(String text) {
         for (Difficulty difficulty : Difficulty.values()) {
             if (text.equalsIgnoreCase(difficulty.toString()))

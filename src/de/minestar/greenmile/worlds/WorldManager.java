fe8697cb2e819517bc334ec3b24f440540deba42
GeMoschen <x-chris-x@arcor.de>
2012-01-19 14:32:49 +0100

    - readded comments to WorldManager.java

--- src/de/minestar/greenmile/worlds/WorldManager.java
+++ src/de/minestar/greenmile/worlds/WorldManager.java
@@ -15,12 +15,22 @@ public class WorldManager {
     private ArrayList<GMWorld> worldList;
     private File dataFolder;
 
+    /**
+     * Constructor
+     * 
+     * @param dataFolder
+     */
     public WorldManager(File dataFolder) {
         this.dataFolder = dataFolder;
         loadWorlds();
     }
 
-    public void addWorld(GMWorld world) {
+    /**
+     * ADD A WORLD TO THE WORLDLIST
+     * 
+     * @param world
+     */
+    private void addWorld(GMWorld world) {
         if (worldExists(world.getWorldName())) {
             ChatUtils.printConsoleWarning("World '" + world.getWorldName() + "' already exists!", Main.name);
             return;
@@ -28,6 +38,15 @@ public class WorldManager {
         this.worldList.add(world);
     }
 
+    /**
+     * CREATE GMWORLD AND BUKKITWORLD
+     * 
+     * @param worldName
+     * @param environment
+     * @param seed
+     * @param dataFolder
+     * @return
+     */
     public boolean createWorld(String worldName, World.Environment environment, long seed, File dataFolder) {
         if (worldExists(worldName)) {
             return false;
@@ -39,6 +58,12 @@ public class WorldManager {
         return result;
     }
 
+    /**
+     * IMPORT WORLD
+     * 
+     * @param worldName
+     * @return
+     */
     public boolean importWorld(String worldName) {
         if (worldExists(worldName)) {
             return false;
@@ -57,6 +82,12 @@ public class WorldManager {
         return true;
     }
 
+    /**
+     * Check if GMWorld exists (by worldName)
+     * 
+     * @param worldName
+     * @return <b>true</b> if the world exists, otherwise <b>false</b>
+     */
     public boolean worldExists(String worldName) {
         for (GMWorld world : this.worldList) {
             if (world.getWorldName().equals(worldName)) {
@@ -66,14 +97,32 @@ public class WorldManager {
         return false;
     }
 
+    /**
+     * Check if GMWorld exists (by block)
+     * 
+     * @param worldName
+     * @return <b>true</b> if the world exists, otherwise <b>false</b>
+     */
     public boolean worldExists(Block block) {
         return worldExists(block.getWorld().getName());
     }
 
+    /**
+     * Check if GMWorld exists (by player)
+     * 
+     * @param worldName
+     * @return <b>true</b> if the world exists, otherwise <b>false</b>
+     */
     public boolean worldExists(Player player) {
         return worldExists(player.getWorld().getName());
     }
 
+    /**
+     * Get the GMWorld by the worldname
+     * 
+     * @param worldName
+     * @return the GMWorld, or null if not found
+     */
     public GMWorld getGMWorld(String worldName) {
         for (GMWorld world : this.worldList) {
             if (world.getWorldName().equals(worldName)) {
@@ -83,28 +132,47 @@ public class WorldManager {
         return null;
     }
 
+    /**
+     * Get the GMWorld by a block
+     * 
+     * @param worldName
+     * @return the GMWorld, or null if not found
+     */
     public GMWorld getGMWorld(Block block) {
         return getGMWorld(block.getWorld().getName());
     }
 
+    /**
+     * Get the GMWorld by a player
+     * 
+     * @param worldName
+     * @return the GMWorld, or null if not found
+     */
     public GMWorld getGMWorld(Player player) {
         return getGMWorld(player.getWorld().getName());
     }
 
+    /**
+     * LOAD WORLDS
+     */
     private void loadWorlds() {
         this.worldList = new ArrayList<GMWorld>();
         try {
             File[] files = this.dataFolder.listFiles();
             for (File f : files) {
                 String fileName = f.getName().trim();
+                // FILESYNTAX: config_%worldname%.yml
                 if ((fileName.endsWith(".yml")) && (fileName.startsWith("config_"))) {
+                    // GET WORLDNAME
                     String worldName = fileName.replace("config_", "");
                     worldName = worldName.substring(0, worldName.length() - 4);
+                    // CREATE GM WORLD
                     GMWorld world = new GMWorld(worldName, this.dataFolder);
                     world.loadSettings(this.dataFolder);
                     if ((!world.getWorldSettings().isInitialized()) || (!world.loadBukkitWorld()))
                         continue;
                     addWorld(world);
+                    // UPDATE BUKKIT WORLD
                     world.updateBukkitWorld();
                 }
             }
@@ -113,10 +181,16 @@ public class WorldManager {
         }
     }
 
+    /**
+     * @return the DataFolder
+     */
     public File getDataFolder() {
         return this.dataFolder;
     }
 
+    /**
+     * @return the Worldlist
+     */
     public ArrayList<GMWorld> getWorldList() {
         return this.worldList;
     }

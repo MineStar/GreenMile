97bcc6cc7480c36f595bcb9c4b9db5cdc35162dc
Stephan Ihling <me@outi-networks.de>
2016-06-29 20:40:13 +0200

    update to 1.9.4

--- src/main/java/de/minestar/greenmile/threading/BorderThread.java
+++ src/main/java/de/minestar/greenmile/threading/BorderThread.java
@@ -5,7 +5,6 @@ import java.util.HashMap;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.World;
-import org.bukkit.entity.Animals;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 
